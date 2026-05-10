18c542bf416021d54cb38db765c1cbe5deb4d46e
rafapcjs <rafaelcorredorgambin1@gmail.com>
2026-05-10 18:04:05 -0500

    feat: update Vite configuration to load environment variables dynamically

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,26 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3018',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+    server: {
+      port: env.PORT ? parseInt(env.PORT) : env.VITE_PORT ? parseInt(env.VITE_PORT) : 5173,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:3018',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     }
   }
